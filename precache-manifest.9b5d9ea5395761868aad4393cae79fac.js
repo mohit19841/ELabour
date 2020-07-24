194b3e138c7c6cfff8a713be790521baed867313
Mohit <mohit.sharma.its2@gmail.com>
2020-07-24 16:18:10 +0530

    Updates

---
+++ precache-manifest.9b5d9ea5395761868aad4393cae79fac.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835330998aaa458586eead60bb30fe75",
+    "url": "/mohit19841/ELabour/index.html"
+  },
+  {
+    "revision": "852f4db1cee5c5a69f34",
+    "url": "/mohit19841/ELabour/static/css/2.d249f610.chunk.css"
+  },
+  {
+    "revision": "aec66c61d640f9caa2a6",
+    "url": "/mohit19841/ELabour/static/css/main.1d013f24.chunk.css"
+  },
+  {
+    "revision": "852f4db1cee5c5a69f34",
+    "url": "/mohit19841/ELabour/static/js/2.7d397afa.chunk.js"
+  },
+  {
+    "revision": "5bbce3bf2978d3ce6fe7b34934f0a4e9",
+    "url": "/mohit19841/ELabour/static/js/2.7d397afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec66c61d640f9caa2a6",
+    "url": "/mohit19841/ELabour/static/js/main.b5e8910f.chunk.js"
+  },
+  {
+    "revision": "17f90a0b13f1c9c87b85",
+    "url": "/mohit19841/ELabour/static/js/runtime-main.0aeb2526.js"
+  },
+  {
+    "revision": "49611f2982d0e8ef61372afe9850535b",
+    "url": "/mohit19841/ELabour/static/media/CircularStd-Black.49611f29.woff"
+  },
+  {
+    "revision": "1c46c3018afa9b775e395b2076943ede",
+    "url": "/mohit19841/ELabour/static/media/CircularStd-BlackItalic.1c46c301.woff"
+  },
+  {
+    "revision": "03e7ee16210905484b09316e8298d712",
+    "url": "/mohit19841/ELabour/static/media/CircularStd-Bold.03e7ee16.woff"
+  },
+  {
+    "revision": "705bbdf2db3c4668c8750804ca12d962",
+    "url": "/mohit19841/ELabour/static/media/CircularStd-BoldItalic.705bbdf2.woff"
+  },
+  {
+    "revision": "829012af195c90c00bac2953653768a8",
+    "url": "/mohit19841/ELabour/static/media/CircularStd-Book.829012af.woff"
+  },
+  {
+    "revision": "3c7409b8c5420ebd6022acae44779108",
+    "url": "/mohit19841/ELabour/static/media/CircularStd-BookItalic.3c7409b8.woff"
+  },
+  {
+    "revision": "381f87a977e09572d69be5e7f6087fe4",
+    "url": "/mohit19841/ELabour/static/media/CircularStd-Medium.381f87a9.woff"
+  },
+  {
+    "revision": "451b6f03fc497603eebc23d24a97bbb0",
+    "url": "/mohit19841/ELabour/static/media/CircularStd-MediumItalic.451b6f03.woff"
+  },
+  {
+    "revision": "523762954bb7a1c25ecc8fe4f5e462fd",
+    "url": "/mohit19841/ELabour/static/media/jobsite.52376295.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mohit19841/ELabour/tree/master/client/precache-manifest.806d2b9f098622f10e9cc93a80e862dd.js"
+  "/mohit19841/ELabour/precache-manifest.9b5d9ea5395761868aad4393cae79fac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mohit19841/ELabour/tree/master/client/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mohit19841/ELabour/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
